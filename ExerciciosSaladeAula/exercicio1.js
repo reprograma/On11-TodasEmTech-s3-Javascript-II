cd4f5b1b34a4f864298ad651d9b17b0cf72c8a8c
Agnes Galvão <agnesgalvao@gmail.com>
2021-03-21 17:17:09 -0300

    Exercicios e desafio finalizados

---
+++ ExerciciosSaladeAula/exercicio1.js
@@ -0,0 +1,18 @@
+
+
+var entrada = Number(prompt( "Digite um numero"));
+
+function somar ( ){
+
+    var numero1 = Number(prompt( "digite um numero"));
+    var numero2 = Number(prompt( "digite um numero"));
+    var soma = numero1-numero2;
+
+    alert(soma )
+      
+
+}
+if ( entrada == 2){
+
+    somar();
+}
