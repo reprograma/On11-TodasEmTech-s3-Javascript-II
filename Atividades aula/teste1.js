482732da71ee7987ce6eb98267d6bcb81bcd18c0
Amanda <bagelha@gmail.com>
2021-03-26 18:17:11 -0300

    realização dos desafios da semana 3

---
+++ Atividades aula/teste1.js
@@ -0,0 +1,130 @@
+/*function somar(){
+    var numero1 = Number(prompt("digite seu primeiro numero:"));
+var numero2 = Number(prompt("digite seu segundo numero:"));
+
+var soma = numero1+numero2
+
+alert(soma);
+}
+
+somar()
+
+
+var entrada =Number(prompt("digite um numero ai"))
+if(entrada==2){
+    somar()
+    
+}*/
+
+
+//exemplo função
+/*function surpresa(){
+    alert('Palhaço')
+}
+surpresa()*/
+
+
+/*function menos(){
+    var num1 = Number(prompt("digite o primeiro numero"))
+    var num2 = Number(prompt("digite o segundo numero"))
+
+    var sub = num1-num2
+
+    alert(sub)
+}
+
+menos()*/
+
+
+
+/*function calcularPromo(){
+    var nomeProduto1 = prompt("digite o nome do produto 1");
+    var valorProduto1 = Number(prompt("digite o valor do produto 1"));
+
+    var nomeProduto2 = prompt("digite o nome do produto 2");
+    var valorProduto2 = Number(prompt("digite o valor do produto 2"));
+
+    var desconto = 5
+    var soma = valorProduto1+valorProduto2
+
+
+    if (nomeProduto1==nomeProduto2 && valorProduto1==valorProduto2){
+        
+        valorPromo = soma-desconto
+
+        alert( valorPromo)
+    }else{
+
+        alert(soma)
+    }    
+}
+
+calcularPromo()*/
+
+/*function confirmarChave(){
+    var chave = Number(prompt("Insira o número correto da chave: "));
+    if(chave==""){
+        return confirmarChave()
+    }
+    if(chave == 1){
+        alert("Chave esta correta")
+    }else{
+        alert("a chava não está correta")
+        return confirmarChave()
+    }
+    }
+confirmarChave()*/
+
+
+/*function mediaAluno(){
+    var nome = prompt("digite o nome aluno");
+    var nota1 = Number(prompt("digite nota1"));
+    var nota2 = Number(prompt("digite nota2"));
+
+    var media = (nota1+nota2)/2;
+
+    if(nome=="" || nota1=="" || nota2==""){
+        return mediaAluno()
+    }
+
+
+    alert ("sua média é"+ media);
+}
+mediaAluno()
+*/
+
+
+/*var numeroChave =  prompt("Qual a chave? ")
+function surpresa(chave){
+    if(chave == 1){
+        alert("Palhaço")
+    }
+}
+
+surpresa(numeroChave)*/
+
+
+//com atribuição de parametro
+/*var nome = prompt("digite o nome aluno");
+var nota1 = Number(prompt("digite nota1"));
+var nota2 = Number(prompt("digite nota2"));
+
+function mediaAluno(nome, nota1, nota2){
+    
+    if(nome=="" || nota1=="" || nota2==""){
+        return mediaAluno()
+    }
+
+    var media = (nota1+nota2)/2;
+
+    alert ("sua média é"+ media);
+}
+mediaAluno(nome, nota1, nota2)*/
+
+// ESCOPO
+
+
+
+
+    
+
